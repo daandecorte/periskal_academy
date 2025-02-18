812114c641c4391b73430c2b83884523de2a88b8
daandecorte <daan.decorte3@gmail.com>
2025-02-18 11:22:54 +0000

    backend werkt

---
+++ app/backend/src/test/java/ap/student/project/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ap.student.project.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
