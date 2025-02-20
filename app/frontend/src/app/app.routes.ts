4ceeb4d1271e6312feb3773fbf07db8bc9c56e91
EvaHautecler <eva.hautecler@outlook.com>
2025-02-20 11:35:43 +0100

    Add Basic routing Paths

--- app/frontend/src/app/app.routes.ts
+++ app/frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ModulesComponent } from './modules/modules.component';
+import { CertificatesComponent } from './certificates/certificates.component';
+import { TipsAndTricksComponent } from './tips-and-tricks/tips-and-tricks.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: "dashboard", component: DashboardComponent},
+    {path: "modules", component: ModulesComponent},
+    {path: "certificates", component: CertificatesComponent},
+    {path: "tips-and-tricks", component: TipsAndTricksComponent},
+    {path: "", redirectTo: "dashboard", pathMatch: 'full'},
+    {path: "**", redirectTo: "dashboard", pathMatch: 'full'}
+];
