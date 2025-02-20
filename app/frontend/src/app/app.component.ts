dfea7663077fee02ba32da9a0fbbb8bac697c72c
Sam Pauwels <sampauwels16@gmail.com>
2025-02-20 11:50:21 +0100

    add navbar to app component

--- app/frontend/src/app/app.component.ts
+++ app/frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { TestService } from './test.service';
 import { firstValueFrom } from 'rxjs';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
