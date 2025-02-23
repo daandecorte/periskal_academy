3cb6138abc28ac2a6bfd8fa600e050cbd35b98f2
daandecorte <daan.decorte3@gmail.com>
2025-02-23 11:08:48 +0100

    nginx.conf added, frontend can reach backend

--- app/frontend/src/app/test.service.ts
+++ app/frontend/src/app/test.service.ts
@@ -8,6 +8,6 @@ export class TestService {
 
   constructor(private httpClient: HttpClient) {}
    GetAll() {
-    return this.httpClient.get("http://academy-142-132-228-124.traefik.me/api/test");
+    return this.httpClient.get("/api/test");
   }
 }
