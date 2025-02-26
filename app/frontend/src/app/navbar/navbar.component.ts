89e3632cef0ace66beeaf935bfc83cc8984f1119
Sam Pauwels <sampauwels16@gmail.com>
2025-02-26 14:23:51 +0100

    add font awseome icons to navbar

--- app/frontend/src/app/navbar/navbar.component.ts
+++ app/frontend/src/app/navbar/navbar.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { 
+  faLayerGroup, 
+  faCertificate, 
+  faLightbulb, 
+  faUsers, 
+  faHeadset, 
+  faSignOutAlt,
+  faUser,
+  faGlobe
+} from '@fortawesome/free-solid-svg-icons';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FontAwesomeModule, NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  faLayerGroup = faLayerGroup;
+  faCertificate = faCertificate;
+  faLightbulb = faLightbulb;
+  faUsers = faUsers;
+  faHeadset = faHeadset;
+  faGlobe = faGlobe;
+  faSignOutAlt = faSignOutAlt;
+  faUser = faUser;
 }
--- app/frontend/src/main.ts
+++ app/frontend/src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { FaIconLibrary, FaConfig } from '@fortawesome/angular-fontawesome';
+import { dom } from '@fortawesome/fontawesome-svg-core';
+
+// Allow Font Awesome to use CSS for styling
+dom.watch();
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
