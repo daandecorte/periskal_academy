9ab79e51869bbe6213bba793f02ade183fb9f8bb
Sam Pauwels <sampauwels16@gmail.com>
2025-03-13 16:11:17 +0100

    add functional search to modules page

--- app/frontend/src/app/modules/modules.component.ts
+++ app/frontend/src/app/modules/modules.component.ts
@@ -3,24 +3,42 @@ import { CommonModule } from '@angular/common';
 import { Module, ModuleService } from '../services/module.service';
 import { ModuleCardComponent } from '../module-card/module-card.component';
 import { AssignedModuleCardComponent } from '../assigned-module-card/assigned-module-card.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-modules',
   standalone: true,
-  imports: [CommonModule, ModuleCardComponent, AssignedModuleCardComponent],
+  imports: [CommonModule, ModuleCardComponent, AssignedModuleCardComponent, FormsModule],
   templateUrl: './modules.component.html',
   styleUrl: './modules.component.css'
 })
 export class ModulesComponent {
   modules: Module[] = [];
   assignedModules: Module[] = [];
+  allModules: Module[] = [];
+  searchTerm: string = '';
 
   constructor(private moduleService: ModuleService) {}
 
   ngOnInit(): void {
     this.moduleService.getModules().subscribe(modules => {
-      this.modules = modules;
-      this.assignedModules = modules.filter(module => module.assigned);
+      this.allModules = modules;
+      this.filterModules();
     });
+  }
+
+  filterModules(): void {
+    const filteredModules = this.searchTerm
+      ? this.allModules.filter(module => 
+          module.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+        )
+      : this.allModules;
+
+    this.modules = filteredModules;
+    this.assignedModules = filteredModules.filter(module => module.assigned);
+  }
+
+  onSearchChange(): void {
+    this.filterModules();
   }
 }
