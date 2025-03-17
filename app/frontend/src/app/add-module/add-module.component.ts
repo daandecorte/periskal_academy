33cde260b6bd1f5bc0cd27fafe650e8535e90d9d
EvaHautecler <eva.hautecler@outlook.com>
2025-03-17 11:01:10 +0100

    Add needed components

---
+++ app/frontend/src/app/add-module/add-module.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-module',
+  imports: [],
+  templateUrl: './add-module.component.html',
+  styleUrl: './add-module.component.css'
+})
+export class AddModuleComponent {
+
+}
---
+++ app/frontend/src/app/add-module/basic-setup/basic-setup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-setup',
+  imports: [],
+  templateUrl: './basic-setup.component.html',
+  styleUrl: './basic-setup.component.css'
+})
+export class BasicSetupComponent {
+
+}
