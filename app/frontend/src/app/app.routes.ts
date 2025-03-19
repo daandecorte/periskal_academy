1da4f671c0dd074fe2a8209fb5e1baf17935be20
Brian Mattheyses <matth@LaPToP_BRiaN.(none)>
2025-03-20 00:47:01 +0100

    skippers management

--- app/frontend/src/app/app.routes.ts
+++ app/frontend/src/app/app.routes.ts
@@ -8,15 +8,48 @@ import { SupportComponent } from './support/support.component';
 import { LoginComponent } from './login/login.component';
 import { Role } from './services/auth.service';
 
-
 export const routes: Routes = [
   { path: 'login', component: LoginComponent }, //To get a role
-  { path: 'modules', component: ModulesComponent , data: { roles: [Role.SKIPPER, Role.INSTALLER, Role.ADMIN, Role.SUPPORT, Role.FLEETMANAGER] }}, //When role is trainee, fleet-manager, helpdesk, admin
-  { path: 'certificates', component: CertificatesComponent , data: { roles: [Role.SKIPPER, Role.INSTALLER, Role.SUPPORT] }}, //When role is trainee, fleet-manager
-  { path: 'tips-and-tricks', component: TipsAndTricksComponent , data: { roles: [Role.SKIPPER, Role.INSTALLER, Role.SUPPORT, Role.FLEETMANAGER] }}, //When role is trainee, fleet-manager, helpdesk
-  { path: 'user-management', component: UserManagementComponent , data: { roles: [Role.ADMIN] }}, //When role is admin
-  { path: 'skippers', component: SkippersComponent , data: { roles: [Role.FLEETMANAGER] }}, //When role is fleet-manager
-  { path: 'support', component: SupportComponent , data: { roles: [Role.SUPPORT] }}, //When role is helpdesk
-  { path: '', redirectTo: 'modules', pathMatch: 'full' }, 
-  { path: '**', redirectTo: 'modules', pathMatch: 'full' },
+  {
+    path: 'modules',
+    component: ModulesComponent,
+    data: {
+      roles: [
+        Role.SKIPPER,
+        Role.INSTALLER,
+        Role.ADMIN,
+        Role.SUPPORT,
+        Role.FLEETMANAGER,
+      ],
+    },
+  }, //When role is trainee, fleet-manager, helpdesk, admin
+  {
+    path: 'certificates',
+    component: CertificatesComponent,
+    data: { roles: [Role.SKIPPER, Role.INSTALLER, Role.SUPPORT] },
+  }, //When role is trainee, fleet-manager
+  {
+    path: 'tips-and-tricks',
+    component: TipsAndTricksComponent,
+    data: {
+      roles: [Role.SKIPPER, Role.INSTALLER, Role.SUPPORT, Role.FLEETMANAGER],
+    },
+  }, //When role is trainee, fleet-manager, helpdesk
+  /*{
+    path: 'user-management',
+    component: UserManagementComponent,
+    data: { roles: [Role.ADMIN] },
+  }, //When role is admin*/
+  {
+    path: 'skippers',
+    component: SkippersComponent,
+    data: { roles: [Role.FLEETMANAGER, Role.SUPPORT, Role.ADMIN] },
+  }, //When role is fleet-manager
+  {
+    path: 'support',
+    component: SupportComponent,
+    data: { roles: [Role.SUPPORT] },
+  }, //When role is helpdesk
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' },
 ];
