930922d3fdf6dea856bdb478ed9ae5f8cf56012c
daandecorte <daan.decorte3@gmail.com>
2025-03-20 13:41:06 +0100

    updated dtos and constructors to match entity classes

--- app/backend/src/main/java/ap/student/project/backend/dto/ChatMemberDTO.java
+++ app/backend/src/main/java/ap/student/project/backend/dto/ChatMemberDTO.java
@@ -1,9 +1,11 @@
 package ap.student.project.backend.dto;
 
+import ap.student.project.backend.entity.Chat;
 import ap.student.project.backend.entity.User;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record ChatMemberDTO(
-        @JsonProperty User user
+        @JsonProperty User user,
+        @JsonProperty Chat chat
 ) {
 }
