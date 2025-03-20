a51503ace280be1d3df09c4f662d026e19d38bf4
EvaHautecler <eva.hautecler@outlook.com>
2025-03-20 16:00:23 +0100

    Add frontend for content and question tabs

--- app/frontend/src/app/add-module/training-form/training-form.component.ts
+++ app/frontend/src/app/add-module/training-form/training-form.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-training-form',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './training-form.component.html',
   styleUrl: './training-form.component.css'
 })
