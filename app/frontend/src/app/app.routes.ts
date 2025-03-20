4a13fd4a1de8cd5fdb822a2dd73289bccd82ccb4
Sam Pauwels <sampauwels16@gmail.com>
2025-03-20 16:36:51 +0100

    add admin modules route and fix duplicate routes

--- app/frontend/src/app/app.routes.ts
+++ app/frontend/src/app/app.routes.ts
@@ -12,6 +12,7 @@ import { BasicSetupComponent } from './add-module/basic-setup/basic-setup.compon
 import { TrainingsComponent } from './add-module/trainings/trainings.component';
 import { ExamComponent } from './add-module/exam/exam.component';
 import { PreviewComponent } from './add-module/preview/preview.component';
+import { AdminModulesComponent } from './admin-modules/admin-modules.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent }, //To get a role
@@ -22,12 +23,14 @@ export const routes: Routes = [
       roles: [
         Role.SKIPPER,
         Role.INSTALLER,
-        Role.ADMIN,
-        Role.SUPPORT,
-        Role.FLEETMANAGER,
       ],
     },
-  }, //When role is trainee, fleet-manager, helpdesk, admin
+  }, //When role is trainee
+  {
+    path: 'admin/modules',
+    component: AdminModulesComponent,
+    data: { roles: [Role.ADMIN] }
+  },
   {
     path: 'certificates',
     component: CertificatesComponent,
@@ -39,49 +42,7 @@ export const routes: Routes = [
     data: {
       roles: [Role.SKIPPER, Role.INSTALLER, Role.SUPPORT, Role.FLEETMANAGER],
     },
-  }, //When role is trainee, fleet-manager, helpdesk
-  /*{
-    path: 'user-management',
-    component: UserManagementComponent,
-    data: { roles: [Role.ADMIN] },
-  }, //When role is admin*/
-  {
-    path: 'skippers',
-    component: SkippersComponent,
-    data: { roles: [Role.FLEETMANAGER, Role.SUPPORT, Role.ADMIN] },
-  }, //When role is fleet-manager
-  {
-    path: 'support',
-    component: SupportComponent,
-    data: { roles: [Role.SUPPORT] },
-  }, //When role is helpdesk
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: '**', redirectTo: 'login', pathMatch: 'full' },
-  {
-    path: 'modules',
-    component: ModulesComponent,
-    data: {
-      roles: [
-        Role.SKIPPER,
-        Role.INSTALLER,
-        Role.ADMIN,
-        Role.SUPPORT,
-        Role.FLEETMANAGER,
-      ],
-    },
-  }, //When role is trainee, fleet-manager, helpdesk, admin
-  {
-    path: 'certificates',
-    component: CertificatesComponent,
-    data: { roles: [Role.SKIPPER, Role.INSTALLER, Role.SUPPORT] },
-  }, //When role is trainee, fleet-manager
-  {
-    path: 'tips-and-tricks',
-    component: TipsAndTricksComponent,
-    data: {
-      roles: [Role.SKIPPER, Role.INSTALLER, Role.SUPPORT, Role.FLEETMANAGER],
-    },
-  }, //When role is trainee, fleet-manager, helpdesk
+  }, 
   {
     path: 'user-management',
     component: UserManagementComponent,
@@ -109,6 +70,4 @@ export const routes: Routes = [
       { path: 'preview', component: PreviewComponent },
     ],
   },
-  { path: '', redirectTo: 'modules', pathMatch: 'full' },
-  { path: '**', redirectTo: 'modules', pathMatch: 'full' },
 ];
