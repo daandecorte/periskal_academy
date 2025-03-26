d520f048b54507fc67b3a398380134721cf87892
Sam Pauwels <sampauwels16@gmail.com>
2025-03-26 17:23:07 +0100

    improve admin modules page look

--- app/frontend/src/app/admin-module-card/admin-module-card.component.ts
+++ app/frontend/src/app/admin-module-card/admin-module-card.component.ts
@@ -1,13 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPencilAlt, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Module } from '../services/module.service';
 
 @Component({
   selector: 'app-admin-module-card',
-  imports: [CommonModule],
+  imports: [
+    CommonModule, 
+    FontAwesomeModule
+  ],
   templateUrl: './admin-module-card.component.html',
   styleUrl: './admin-module-card.component.css'
 })
 export class AdminModuleCardComponent {
   @Input() module!: Module;
+
+  faPencilAlt = faPencilAlt;
+  faTrash = faTrash;
 }
