f08f9816c4129b368804aae874b437764bdd1e5c
daandecorte <daan.decorte3@gmail.com>
2025-03-26 17:22:22 +0100

    for securing pages added auth guard

---
+++ app/frontend/src/app/auth.guard.ts
@@ -0,0 +1,17 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { inject } from '@angular/core';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  console.log("Checking authentication status...");
+
+  if (authService.currentUserValue != null) {
+    return true;
+  } else {
+    router.navigate(['/login']);
+    return false;
+  }
+};
