095714bd0bf21f084ed9608521e5a01161a1aad6
EvaHautecler <eva.hautecler@outlook.com>
2025-03-27 15:59:17 +0100

    Add languages to basic info

--- app/frontend/src/app/admin-edit-module/admin-edit-module.component.ts
+++ app/frontend/src/app/admin-edit-module/admin-edit-module.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-admin-edit-module',
-  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, TranslateModule],
   templateUrl: './admin-edit-module.component.html',
   styleUrl: './admin-edit-module.component.css'
 })
