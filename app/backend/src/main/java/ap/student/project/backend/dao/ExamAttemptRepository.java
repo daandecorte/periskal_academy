fa5c34dbedbd5a3a4df2f22cb4b2399ad3390ec7
daandecorte <daan.decorte3@gmail.com>
2025-03-29 21:41:26 +0100

    removed some unnecessary auto-generated endpoints

--- app/backend/src/main/java/ap/student/project/backend/dao/ExamAttemptRepository.java
+++ app/backend/src/main/java/ap/student/project/backend/dao/ExamAttemptRepository.java
@@ -2,8 +2,10 @@ package ap.student.project.backend.dao;
 
 import ap.student.project.backend.entity.ExamAttempt;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
 
-@Repository
+@RepositoryRestResource(exported = false)
 public interface ExamAttemptRepository extends ListCrudRepository<ExamAttempt, Integer> {
 }
--- app/backend/src/main/java/ap/student/project/backend/dao/UserRepository.java
+++ app/backend/src/main/java/ap/student/project/backend/dao/UserRepository.java
@@ -2,11 +2,12 @@ package ap.student.project.backend.dao;
 
 import ap.student.project.backend.entity.User;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+@RepositoryRestResource(exported = false)
 public interface UserRepository extends ListCrudRepository<User, Integer> {
     User findByPeriskalId(String periskalId);
     boolean existsByPeriskalId(String periskalId);
