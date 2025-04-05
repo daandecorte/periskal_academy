c9557d61e56dd6099054b5d776667a5f79491b2e
Brian Mattheyses <matth@LaPToP_BRiaN.(none)>
2025-04-05 22:12:01 +0200

    tips and tricks all add delete and edit UI

--- app/frontend/src/app/tips-and-tricks/tips-and-tricks.component.ts
+++ app/frontend/src/app/tips-and-tricks/tips-and-tricks.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import {
   faGlobe,
   faInfoCircle,
@@ -20,7 +20,7 @@ import { Observable } from 'rxjs';
   templateUrl: './tips-and-tricks.component.html',
   styleUrl: './tips-and-tricks.component.css',
 })
-export class TipsAndTricksComponent {
+export class TipsAndTricksComponent implements AfterViewInit {
   faGlobe = faGlobe;
   faInfo = faInfoCircle;
   faAngleUp = faAngleUp;
@@ -34,7 +34,20 @@ export class TipsAndTricksComponent {
 
   currentUser$: Observable<IUser | null>;
   currentUserRole: string | null = null;
-  editorUser: boolean = false;
+  editorUser: boolean = true; // AANPASSEN
+
+  modalOverlay!: HTMLDivElement;
+  modalOverlayDelete!: HTMLDivElement;
+
+  saveModalBtn!: HTMLButtonElement;
+  closeModalBtn!: HTMLButtonElement;
+
+  titleAdd: boolean = false;
+  titleEdit: boolean = false;
+  titleDelete: boolean = false;
+  tipAdd: boolean = false;
+  tipEdit: boolean = false;
+  tipDelete: boolean = false;
 
   constructor(private authService: AuthService) {
     this.currentUser$ = this.authService.currentUser$;
@@ -50,7 +63,153 @@ export class TipsAndTricksComponent {
     }
   }
 
+  ngAfterViewInit() {
+    this.modalOverlay = document.getElementById(
+      'modal-overlay'
+    ) as HTMLDivElement;
+    this.modalOverlayDelete = document.getElementById(
+      'modal-overlay-delete'
+    ) as HTMLDivElement;
+    this.closeModalBtn = document.getElementById(
+      'close-modal'
+    ) as HTMLButtonElement;
+
+    let editTitleIcons = document.getElementsByClassName(
+      'edit-title'
+    ) as HTMLCollectionOf<HTMLElement>;
+
+    for (let i = 0; i < editTitleIcons.length; i++) {
+      editTitleIcons[i].addEventListener('click', () => {
+        this.editTitleShow();
+      });
+    }
+
+    let deleteTopicIcons = document.getElementsByClassName(
+      'delete-topic'
+    ) as HTMLCollectionOf<HTMLElement>;
+
+    for (let i = 0; i < deleteTopicIcons.length; i++) {
+      deleteTopicIcons[i].addEventListener('click', () => {
+        this.deleteTopicShow();
+      });
+    }
+
+    let addTipIcons = document.getElementsByClassName(
+      'add-tip'
+    ) as HTMLCollectionOf<HTMLElement>;
+
+    for (let i = 0; i < addTipIcons.length; i++) {
+      addTipIcons[i].addEventListener('click', () => {
+        this.addTipShow();
+      });
+    }
+
+    let editTipIcons = document.getElementsByClassName(
+      'edit-tip'
+    ) as HTMLCollectionOf<HTMLElement>;
+
+    for (let i = 0; i < editTipIcons.length; i++) {
+      editTipIcons[i].addEventListener('click', () => {
+        this.editTipShow();
+      });
+    }
+
+    let deleteTipIcons = document.getElementsByClassName(
+      'delete-tip'
+    ) as HTMLCollectionOf<HTMLElement>;
+
+    for (let i = 0; i < deleteTipIcons.length; i++) {
+      deleteTipIcons[i].addEventListener('click', () => {
+        this.deleteTipShow();
+      });
+    }
+
+    const buttons =
+      document.querySelectorAll<HTMLButtonElement>('.modal-button');
+
+    buttons.forEach((button) => {
+      button.addEventListener('click', () => {
+        buttons.forEach((btn) => btn.classList.remove('selected'));
+
+        button.classList.add('selected');
+      });
+    });
+  }
+
+  closeModal() {
+    const tipTitle = document.getElementById('tip-title') as HTMLInputElement;
+    const tipText = document.getElementById(
+      'tip-content'
+    ) as HTMLTextAreaElement;
+
+    if (tipText) tipText.value = '';
+    if (tipTitle) tipTitle.value = '';
+
+    this.modalOverlay.classList.add('hidden');
+  }
+
+  saveTopic() {}
+
+  saveTip() {}
+
+  addTopicShow() {
+    this.modalOverlay.classList.remove('hidden');
+    this.titleAdd = true;
+    this.titleEdit = false;
+    this.tipAdd = false;
+    this.tipEdit = false;
+  }
+
+  editTitleShow() {
+    this.modalOverlay.classList.remove('hidden');
+    this.titleAdd = false;
+    this.titleEdit = true;
+    this.tipAdd = false;
+    this.tipEdit = false;
+  }
+
+  addTipShow() {
+    this.modalOverlay.classList.remove('hidden');
+    this.titleAdd = false;
+    this.titleEdit = false;
+    this.tipAdd = true;
+    this.tipEdit = false;
+  }
+
+  editTipShow() {
+    this.modalOverlay.classList.remove('hidden');
+    this.titleAdd = false;
+    this.titleEdit = false;
+    this.tipAdd = false;
+    this.tipEdit = true;
+  }
+
+  closeModalDelete() {
+    this.modalOverlayDelete.classList.add('hidden');
+  }
+
+  deleteTopicShow() {
+    this.modalOverlayDelete.classList.remove('hidden');
+
+    this.titleDelete = true;
+    this.tipDelete = false;
+  }
+
+  deleteTipShow() {
+    this.modalOverlayDelete.classList.remove('hidden');
+
+    this.titleDelete = false;
+    this.tipDelete = true;
+  }
+
   toggleSection(event: Event, index: number): void {
+    if (
+      (this.tipAdd || this.titleEdit || this.titleDelete) &&
+      (!this.modalOverlay.classList.contains('hidden') ||
+        !this.modalOverlayDelete.classList.contains('hidden'))
+    )
+      return;
+
     const header = event.currentTarget as HTMLElement;
     header.classList.toggle('collapsed');
 
