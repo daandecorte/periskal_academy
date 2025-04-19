0ba59dae8f2625b81cdf5bc2b559fa73da8d3d9a
Sam Pauwels <s142765@ap.be>
2025-04-19 15:09:20 +0200

    add module video view component

---
+++ app/frontend/src/app/module-video-view/module-video-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-module-video-view',
+  imports: [],
+  templateUrl: './module-video-view.component.html',
+  styleUrl: './module-video-view.component.css'
+})
+export class ModuleVideoViewComponent {
+
+}
