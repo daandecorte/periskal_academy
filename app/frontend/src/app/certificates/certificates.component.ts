8a79ce99e88b6c51fe8a7634e6e754c9b2668da9
daandecorte <daan.decorte3@gmail.com>
2025-04-24 16:25:58 +0200

    billing info screen

--- app/frontend/src/app/certificates/certificates.component.ts
+++ app/frontend/src/app/certificates/certificates.component.ts
@@ -33,4 +33,7 @@ export class CertificatesComponent {
   onUpdateAcceptTerms(accepted: boolean) {
     this.termsAccepted = accepted;
   }
+  complete() {
+    this.router.navigate(["/trainings"])
+  }
 }
