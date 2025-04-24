2ec93829034455713277ea085be58f4f957d03d5
daandecorte <daan.decorte3@gmail.com>
2025-04-24 14:01:35 +0200

    navigation through certificate steps

--- app/frontend/src/app/certificates/certificates.component.ts
+++ app/frontend/src/app/certificates/certificates.component.ts
@@ -6,10 +6,12 @@ import { Subscription } from 'rxjs';
 import { AssignSailorComponent } from "./assign-sailor/assign-sailor.component";
 import { AcceptTermsComponent } from "./accept-terms/accept-terms.component";
 import { SendInfoComponent } from "./send-info/send-info.component";
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificates',
-  imports: [SelectModuleComponent, TraineeChatComponent, AssignSailorComponent, AcceptTermsComponent, SendInfoComponent],
+  imports: [FormsModule, CommonModule, SelectModuleComponent, TraineeChatComponent, AssignSailorComponent, AcceptTermsComponent, SendInfoComponent],
   templateUrl: './certificates.component.html',
   styleUrl: './certificates.component.css'
 })
@@ -24,6 +26,10 @@ export class CertificatesComponent {
 
   nextStep() {
     this.currentIndex++;
+    this.router.navigate([`/certificates/${this.paths[this.currentIndex]}`]);
+  }
+  previousStep() {
+    this.currentIndex--;
     this.router.navigate([`/certificates/${this.paths[this.currentIndex]}`]);
   }
 }
