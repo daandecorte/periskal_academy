3a4e37244dccc6813f44f90cd5c3b9e1ae26dbd6
daandecorte <daan.decorte3@gmail.com>
2025-04-24 14:28:56 +0200

    select sailor screen

--- app/frontend/src/app/certificates/assign-sailor/assign-sailor.component.ts
+++ app/frontend/src/app/certificates/assign-sailor/assign-sailor.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-assign-sailor',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './assign-sailor.component.html',
   styleUrl: './assign-sailor.component.css'
 })
 export class AssignSailorComponent {
-
+  faUser=faUser;
 }
