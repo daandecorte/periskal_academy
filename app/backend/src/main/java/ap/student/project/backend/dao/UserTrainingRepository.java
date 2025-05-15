d2e2cd1de51d78b1e5eb897a449459eb54dd255c
daandecorte <daan.decorte3@gmail.com>
2025-05-15 14:09:29 +0200

    backend controller and service for finding userTraingings

--- app/backend/src/main/java/ap/student/project/backend/dao/UserTrainingRepository.java
+++ app/backend/src/main/java/ap/student/project/backend/dao/UserTrainingRepository.java
@@ -4,6 +4,9 @@ import ap.student.project.backend.entity.UserTraining;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
 @RepositoryRestResource(exported = false)
 public interface UserTrainingRepository extends JpaRepository<UserTraining, Integer> {
+    Optional<UserTraining> findByTrainingIdAndUserId(int trainingId, int userId);
 }
