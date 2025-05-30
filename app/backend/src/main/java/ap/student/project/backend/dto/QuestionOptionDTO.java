59231e1e8fbcbc6bb9e678a5fc6c71c1c6ddd8f3
daandecorte <daan.decorte3@gmail.com>
2025-05-30 12:33:30 +0200

    removed questionoption, moduleprogress and userexam dtos since they were not used

--- app/backend/src/main/java/ap/student/project/backend/dto/QuestionOptionDTO.java
+++
@@ -1,7 +0,0 @@
-package ap.student.project.backend.dto;
-
-public record QuestionOptionDTO(
-        //@JsonProperty("text") String text,
-        //@JsonProperty("is_correct") boolean isCorrect
-) {
-}
